5bd6a8f78ef2f5d95e4ee10a38a5f513b8f03730
salitha10 <salithaniranjana@gmail.com>
2021-06-30 23:33:51 +0530

    Frontend test

---
+++ frontend/src/components/CounterClass.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class CounterClass extends React.Component {
+
+    //Constructor
+    constructor(){
+
+        super();
+        //Bind functions
+        this.increment = this.increment.bind(this)
+
+        //State
+        this.state = {
+            number: 0
+        };
+    }
+
+    //function to increment value
+    increment(){
+        this.setState({
+            number: this.state.number + 1
+        })
+    }
+
+
+
+
+    //Render elements
+    render(){
+        return (
+
+            <div>
+                <h1> Counter = {this.state.number} </h1>
+                <button onClick={this.increment}>Increment</button>
+            </div>
+
+        )
+    }
+
+}
+
+export default CounterClass;
