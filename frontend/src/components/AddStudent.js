edd8acf7deb19cfa05a55dc4a4ba20bb02593f8c
salitha10 <salithaniranjana@gmail.com>
2021-07-03 01:22:33 +0530

    Add done

--- frontend/src/components/AddStudent.js
+++ frontend/src/components/AddStudent.js
@@ -1,21 +1,40 @@
-import React, {useSate} from 'react'
+import React, {useState} from 'react';
+import axios from 'axios';
 
 export default function AddStudent(){
  
-    const [name, setName] = setState('');
-    const [age, setAge] = setState('');
-    const [gender, setGender] = setState('');
-    const [address, setAddress] = setState('');
+    const [name, setName] = useState("");
+    const [age, setAge] = useState("");
+    const [gender, setGender] = useState("");
+    const [address, setAddress] = useState("");
+
+    function sendData(e){
+        e.preventDefault();
+
+        //Data object
+        const newStudent = {
+            name,
+            age,
+            gender,
+            address
+        }
+
+       axios.post('http://localhost:5000/student/add', newStudent).then(()=>{
+           alert('Student added');
+        }).catch((err) =>{
+            alert('Insert Failed');
+        });
+}
 
     return(
 
-<div class="container">
+<div className="container">
 
-<div class="card" style={{padding:"20px" }}>
-    <form>
-        <div class="form-group">
-            <label for="name">Student Name</label>
-            <input type="text" class="form-control" id="name" placeholder="Enter Name"
+<div className="card" style={{padding:"20px" }}>
+    <form onSubmit={sendData}>
+        <div className="form-group">
+            <label htmlFor="name">Student Name</label>
+            <input type="text" className="form-control" id="name" placeholder="Enter Name"
             
             onChange={(e)=>{
                 setName(e.target.value)
@@ -24,9 +43,9 @@ export default function AddStudent(){
             ></input>
         </div>
 
-        <div class="form-group">
-            <label for="age">Student Age</label>
-            <input type="number"  min="0" step="1" class="form-control" id="age" placeholder="Age"
+        <div className="form-group">
+            <label htmlFor="age">Student Age</label>
+            <input type="number"  min="0" step="1" className="form-control" id="age" placeholder="Age"
             
             onChange={(e)=>{
                 setAge(e.target.value)
@@ -35,9 +54,9 @@ export default function AddStudent(){
             ></input>
         </div>
 
-        <div class="form-group">
-            <label for="gender">Student Gender</label>
-            <input type="password" class="form-control" id="gender" placeholder="Gender"
+        <div className="form-group">
+            <label htmlFor="gender">Student Gender</label>
+            <input type="text" className="form-control" id="gender" placeholder="Gender"
             
             onChange={(e)=>{
                 setGender(e.target.value)
@@ -46,9 +65,9 @@ export default function AddStudent(){
             ></input>
         </div>
 
-        <div class="form-group">
-            <label for="adress">Student Address</label>
-            <input type="password" class="form-control" id="address" placeholder="Address"
+        <div className="form-group">
+            <label htmlFor="adress">Student Address</label>
+            <input type="text" className="form-control" id="address" placeholder="Address"
             
             onChange={(e)=>{
                 setAddress(e.target.value)
@@ -57,7 +76,7 @@ export default function AddStudent(){
             ></input>
         </div>
 
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" className="btn btn-primary">Submit</button>
     </form>
     </div>
 </div>
